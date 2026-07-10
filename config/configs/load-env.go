8785640d247db2e07e85b6498143a3e90a93b2fd
deepak26ba <deepak26ba@gmail.com>
2026-07-10 15:28:42 +0530

    Add AutoMigrate support to database configuration and update example environment file

--- config/configs/load-env.go
+++ config/configs/load-env.go
@@ -10,12 +10,13 @@ type Config struct {
 }
 
 type DatabaseConfig struct {
-	Host     string
-	Port     string
-	Username string
-	Password string
-	Name     string
-	SSLMode  string
+	Host        string
+	Port        string
+	Username    string
+	Password    string
+	Name        string
+	SSLMode     string
+	AutoMigrate string
 }
 
 type HTTPConfig struct {
@@ -28,19 +29,23 @@ func LoadEnv() *Config {
 
 	return &Config{
 		Database: DatabaseConfig{
-			Host:     getEnv("DB_HOST"),
-			Port:     getEnv("DB_PORT"),
-			Username: getEnv("DB_USERNAME"),
-			Password: getEnv("DB_PASSWORD"),
-			Name:     getEnv("DB_NAME"),
-			SSLMode:  getEnv("DB_SSL_MODE"),
+			Host:        getEnv("DB_HOST",""),
+			Port:        getEnv("DB_PORT","5432"),
+			Username:    getEnv("DB_USERNAME",""),
+			Password:    getEnv("DB_PASSWORD",""),
+			Name:        getEnv("DB_NAME",""),
+			SSLMode:     getEnv("DB_SSL_MODE",""),
+			AutoMigrate: getEnv("DB_AUTOMIGRATE","false"),
 		},
 		HTTP: HTTPConfig{
-			Port: getEnv("HTTP_PORT"),
+			Port: getEnv("HTTP_PORT","6369"),
 		},
 	}
 }
 
-func getEnv(key string) string {
-	return os.Getenv(key)
+func getEnv(key, defaultValue string) string {
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+	return defaultValue
 }
